d92c33042292cce7040fea3e96917d7cdaa90d41
Zouhair Begdar <parkour@ora.ma>
2024-05-17 12:20:34 +0100

    day 1 create a list of days in our app

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -30,7 +30,6 @@ export default function RootLayout() {
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
       </Stack>
     </ThemeProvider>
   );
---
+++ components/dayListItem/DayListItem.tsx
@@ -0,0 +1,36 @@
+import { Text, View, StyleSheet, Pressable } from 'react-native';
+import { Link } from 'expo-router';
+
+type DayListItem = {
+  day: number;
+};
+
+export default function DayListItem({ day }: DayListItem) {
+  return (
+    <Link href={`/day${day}`} asChild>
+      <Pressable style={styles.box}>
+        <Text style={styles.text}>{day}</Text>
+      </Pressable>
+    </Link>
+  );
+}
+
+const styles = StyleSheet.create({
+  box: {
+    backgroundColor: '#F9EDE3',
+    flex: 1,
+    aspectRatio: 1,
+
+    borderWidth: StyleSheet.hairlineWidth,
+    borderColor: '#9b4521',
+    borderRadius: 20,
+
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    color: '#9b4521',
+    fontSize: 20,
+    fontFamily: 'AmaticBold',
+  },
+});
