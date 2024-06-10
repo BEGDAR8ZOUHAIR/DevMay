95fca49fc60606e6c79c24331fa8ccad13eeceb0
Zouhair Begdar <parkour@ora.ma>
2024-06-10 15:03:00 +0100

    add shake animation using reanimated

--- app/(days)/day5/index.tsx
+++ app/(days)/day5/index.tsx
@@ -1,54 +1,82 @@
-import { View, Text, StyleSheet, Button } from 'react-native'
-import React from 'react'
-import Header from '@/components/header/Header';
-import { Ionicons, Octicons } from '@expo/vector-icons';
-import { Link, useNavigation, useRouter } from "expo-router";
-import { Stack } from 'expo-router';
-import MarkdownDisplay from '@/components/day3/MarkdownDisplay';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Entypo } from '@expo/vector-icons';
+import { useCallback, useState } from 'react';
+import Animated, {
+  useAnimatedStyle,
+  withTiming,
+} from 'react-native-reanimated';
+import { useAnimatedShake } from '../../../hooks/use-animated-shake';
 
+export default function App() {
+  const [count, setCount] = useState(0);
 
-const description = `
-# Shake Animated View
-`;
+  const { shake, rStyle, isShaking } = useAnimatedShake();
 
-const DayDetailsScreen = () => {
-	const router = useRouter();
-  const navigation = useNavigation();
-	return (
-		<View style={styles.container}>
-			<Stack.Screen options={{ headerShown: false }} />
-			<Header
-				leftButton={{ child: <Ionicons name="arrow-back-outline" size={30} color="#9b4521" />, onPress: () => navigation.goBack() }}
-				rightButton={{ child: <Octicons name="info" size={30} color="#9b4521" />, onPress: () => alert('Info') }}
-				middleButton={{ child: <Text style={{ fontSize: 25, fontFamily: 'SpaceMono' }}>Day 5</Text> }}
-			/>
-			<MarkdownDisplay>{description}</MarkdownDisplay>
-			<View style={styles.button}>
-			<Link href="/day5/airbnb" asChild >
-        <Button  title="Go to Airbnb" />
-      </Link>
-			</View>
-		</View>
-	)
+  const onIncrement = useCallback(() => {
+    setCount((c) => c + 1);
+  }, []);
+
+  const onDecrement = useCallback(() => {
+    setCount((c) => {
+      if (c === 0) {
+        shake();
+        return 0;
+      }
+      return c - 1;
+    });
+  }, []);
+
+  const rErrorTextStyle = useAnimatedStyle(() => {
+    return {
+      color: withTiming(isShaking.value ? 'red' : 'black', {
+        duration: 50,
+      }),
+    };
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <StatusBar style="auto" />
+      <Animated.Text style={[styles.counterText, rStyle, rErrorTextStyle]}>
+        {count}
+      </Animated.Text>
+      <View style={styles.buttonsContainer}>
+        <TouchableOpacity style={styles.button} onPress={onDecrement}>
+          <Entypo name="minus" size={32} color="white" />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={onIncrement}>
+          <Entypo name="plus" size={32} color="white" />
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
 }
 
-export default DayDetailsScreen
-
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-    paddingTop: 50,
-		
-	},
-	text: {
-		fontSize: 44,
-		fontWeight: 'bold',
-		fontFamily: 'AmaticSC_Bold',
-	},
-
-	button: {
-		bottom: 100
-
-	}
-
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  counterText: {
+    fontSize: 98,
+    fontWeight: 'bold',
+  },
+  buttonsContainer: {
+    position: 'absolute',
+    bottom: 64,
+    right: 32,
+    flexDirection: 'row',
+    gap: 8,
+  },
+  button: {
+    height: 64,
+    aspectRatio: 1,
+    backgroundColor: '#0f41a4',
+    borderRadius: 32,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
 });
